a512950226fa4370c81c875ca49ebcbcb8c94159
salemzinho <luan_co30@hotmail.com>
2022-04-09 01:35:24 -0300

    git init

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="PT-BR">
+<head>
+
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+
+    <title>Salemzinho Portfólio</title>
+
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@500&display=swap" rel="stylesheet">
+
+    <!-- Boostrap -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+
+    <link rel="stylesheet" href="assets/css/main.css">
+
+</head>
+<body>
+
+<div class="jumbotron banner-home">
+    <nav class="navbar navbar-expand-lg shadow-sm">
+        <div class="container">
+            <a class="navbar-brand me-2 mt-1" href="/">
+                <h4 id="logo"><b class="cor-verde">Salem</b>zinho</h4>
+            </a> 
+            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="navbar-collapse collapse justify-content-end" id="navbar">
+                <ul class="navbar-nav mr-auto">
+                    <div class="d-flex">
+                    <a href="/sobre-nos.php">
+
+                    </a>
+                    <a href="https://nubank.com.br/pagar/1590nb/jjMH50j5e0">
+                    </a>
+                    <a class="btn btn-dark px-3" href="https://github.com/Salemzinho" role="button"><i class="fab fa-github"></i></a>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <br><br><br><br><br>
+
+    <div class="container">
+        <h2>Bem vindo!</h2>
+        <h1>Me chamo <b class="cor-verde">Luan Costa</b></h1>
+        <h3 class="typewriter">E sou um <b class="cor-verde">Desenvolvedor</b></h3>
+        <hr class="my-4">
+        <p>Vem dar uma olhada nos projetos que andei realizando por ai :)</p>
+        <button class="mt-5" id="btn-banner">
+            <span>Clique aqui</span>
+        </button>
+    </div>
+
+    <br><br><br><br><br><br><br><br><br><br>
+</div>
+
+
+<!-- criar cards em acrilico -->
+</body>
+</html>
