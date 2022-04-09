4712ce8ca4d8a92d18a3adde45ad8f6e6eebacf2
salemzinho <luan_co30@hotmail.com>
2022-04-09 03:19:39 -0300

    Section de Projetos

--- index.php
+++ index.php
@@ -9,6 +9,9 @@
 
     <title>Salemzinho Portfólio</title>
 
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/all.min.js" integrity="sha512-6PM0qYu5KExuNcKt5bURAoT6KCThUmHRewN3zUFNaoI6Di7XJPTMoT6K0nsagZKk2OB4L7E3q1uQKHNHd4stIQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/js/fontawesome.min.js" integrity="sha512-5qbIAL4qJ/FSsWfIq5Pd0qbqoZpk5NcUVeAAREV2Li4EKzyJDEGlADHhHOSSCw0tHP7z3Q4hNHJXa81P92borQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.1.1/css/all.min.css" integrity="sha512-KfkfwYDsLkIlwQp6LFnl8zNdLGxu9YAA1QvwINks4PhcElQSvqcyVLLD9aMhXd13uQjoXtEKNosOWaZqXgel0g==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -25,46 +28,104 @@
 <body>
 
 <div class="jumbotron banner-home">
-    <nav class="navbar navbar-expand-lg shadow-sm">
+    <nav class="navbar navbar-expand-lg bg-navbar shadow-sm fixed-top">
         <div class="container">
             <a class="navbar-brand me-2 mt-1" href="/">
                 <h4 id="logo"><b class="cor-verde">Salem</b>zinho</h4>
             </a> 
-            <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbar">
-                <i class="fas fa-bars"></i>
+            <button class="navbar-toggler bg-dark" data-bs-toggle="collapse" data-bs-target="#navbar">
+                <i class="fas fa-bars" style="color: white;"></i>
             </button>
             <div class="navbar-collapse collapse justify-content-end" id="navbar">
                 <ul class="navbar-nav mr-auto">
-                    <div class="d-flex">
-                    <a href="/sobre-nos.php">
-
-                    </a>
-                    <a href="https://nubank.com.br/pagar/1590nb/jjMH50j5e0">
-                    </a>
+                    <li class="m-auto">
+                        <a href="#projetos" class="me-4 text-white efeito-hover-menu">
+                            Projetos
+                        </a>
+                    </li>
+                    <li class="m-auto">
+                        <a href="#redes-sociais" class="me-4 text-white efeito-hover-menu">
+                            Redes Sociais
+                        </a>
+                    </li>
                     <a class="btn btn-dark px-3" href="https://github.com/Salemzinho" role="button"><i class="fab fa-github"></i></a>
-                    </div>
+
                 </ul>
             </div>
         </div>
     </nav>
 
-    <br><br><br><br><br>
+    <br><br><br><br><br><br><br>
 
     <div class="container">
         <h2>Bem vindo!</h2>
         <h1>Me chamo <b class="cor-verde">Luan Costa</b></h1>
         <h3 class="typewriter">E sou um <b class="cor-verde">Desenvolvedor</b></h3>
-        <hr class="my-4">
+        <hr class="my-4 col-6">
         <p>Vem dar uma olhada nos projetos que andei realizando por ai :)</p>
-        <button class="mt-5" id="btn-banner">
+        <button class="mt-5 btn-identidade">
             <span>Clique aqui</span>
         </button>
     </div>
 
     <br><br><br><br><br><br><br><br><br><br>
+
 </div>
 
 
-<!-- criar cards em acrilico -->
+<div class="jumbotron section-projetos" id="projetos">
+    <div class="container">
+    <br><br>
+    <h2 class="text-white text-center mt-5">Meus Projetos</h2>
+    <br><br>
+        <div class="mt-5 col-12">
+            <div class="row">
+                <div class="col-sm-4 mb-5">
+                    <div class="card card-projeto p-3 rounded-0">
+                        <div class="card-body">
+                            <h5 class="text-left text-white mb-5">blueDenounces</h5>
+                            <p class="card-text text-muted">💙 Website voltado para denunciar de forma menos burocrática os usuários que compartilham pornografia infantil no Twitter.</p>
+                            <button class="mt-4 col-12 btn-identidade-card">
+                                <span>GitHub</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-5">
+                    <div class="card card-projeto p-3 rounded-0">
+                        <div class="card-body">
+                            <h5 class="text-left text-white mb-5">LaraCRUD</h5>
+                            <p class="card-text text-muted">👨🏻‍💻 O LaraCRUD é uma reimaginação do Crud feito em Laravel baseado no projeto realizado por Douglas Carlos Men nos artigos da Imasters.</p>
+                            <button class="mt-4 col-12 btn-identidade-card">
+                                <span>GitHub</span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-4 mb-5">
+                    <div class="card card-projeto p-3 rounded-0">
+                        <div class="card-body">
+                            <h5 class="text-left text-white mb-5">ImóvelP</h5>
+                            <p class="card-text text-muted">🧡 Proptech que descomplica a preparação e divulgação do imóvel para o proprietário e a forma de captar imóveis para corretores. Atuo nela como desenvolvedor estágiario. </p>
+                            <button class="mt-4 col-12 btn-identidade-card">
+                                <span>Site <i class="fa-regular fa-rocket-launch"></i></span>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="jumbotron section-redes-sociais" id="redes-sociais">
+    <div class="container">
+    <br><br>
+    <h2 class="text-white text-center">Minhas Redes Sociais</h2>
+     
+    </div>
+</div>
+
+
 </body>
 </html>
